091a20b09d6f3d77c50c9311dbf10768cd054510
bbednarek <b@finmid.com>
2024-09-17 17:26:42 +0300

    refactor

--- src/main/kotlin/com/finmid/fp/kata/Game.kt
+++ src/main/kotlin/com/finmid/fp/kata/Game.kt
@@ -28,11 +28,6 @@ fun calculateNextPosition(
         Direction.LEFT -> position.copy(x = position.x - 1)
     }
 
-fun validateMovement(
-    map: Map,
-    direction: Direction,
-): Boolean = validateWalls(map, direction) && validateTrees(map, direction)
-
 fun validateMovement2(
     map: Map,
     direction: Direction,
@@ -40,25 +35,11 @@ fun validateMovement2(
     !isOutsideWalls(it) && !hitTheTree(it)
 }
 
-private fun validateWalls(map: Map, direction: Direction) = when (direction) {
-    Direction.UP -> map.robot.y > 0
-    Direction.DOWN -> map.robot.y < map.height - 1
-    Direction.RIGHT -> map.robot.x < map.width - 1
-    Direction.LEFT -> map.robot.x > 0
-}
-
 fun isOutsideWalls(map: Map) : Boolean =
      map.robot.y < 0 ||
      map.robot.y > map.height - 1 ||
      map.robot.x > map.width - 1 ||
      map.robot.x < 0
-
-fun validateTrees(
-    map: Map,
-    direction: Direction,
-): Boolean = calculateNextPosition(map.robot, direction).let { robotNewPosition ->
-    map.obstacles.none { robotNewPosition == it }
-}
 
 fun hitTheTree(
     map: Map,
