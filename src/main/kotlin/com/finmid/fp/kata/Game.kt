416a6d043ae78d1f41ebc422fe2c1959a4af0c51
Dmitrii Barnukov <dmitrii@finmid.com>
2024-09-17 15:40:24 +0200

    Added base

--- src/main/kotlin/com/finmid/fp/kata/Game.kt
+++ src/main/kotlin/com/finmid/fp/kata/Game.kt
@@ -4,4 +4,19 @@ package com.finmid.fp.kata
 fun moveRobot(
     textMap: String,
     moveCommands: String,
-): String = textMap
+): String = parseMap(textMap).applyCommands(parseCommands(moveCommands)).serialize()
+
+private fun Map.applyCommands(parsedCommands: List<Direction>): Map = parsedCommands
+    .fold(this) { map, command ->
+        map.applyCommand(command)
+    }
+
+private fun Map.applyCommand(command: Direction): Map = when (command) {
+    Direction.UP -> this.copy(robot = this.robot.copy(y = this.robot.y + 1))
+    Direction.DOWN -> this.copy(robot = this.robot.copy(y = this.robot.y - 1))
+    Direction.LEFT -> this.copy(robot = this.robot.copy(x = this.robot.x - 1))
+    Direction.RIGHT -> this.copy(robot = this.robot.copy(x = this.robot.x + 1))
+}
+
+
+private fun Map.serialize(): String = serialiseMap(this)
